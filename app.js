4355ee5e1c6dc4e61140d49b3d44b952df9e0b8f
Dan Misael Zepeda <112110721+MisaelZepeda@users.noreply.github.com>
2026-04-14 14:32:25 -0600

    Actualizar app.js

--- app.js
+++ app.js
@@ -397,4 +397,4 @@ async function generarPDFMes() {
 
 
 
-if ('serviceWorker' in navigator) { navigator.serviceWorker.register('sw.js'); }
+if ('serviceWorker' in navigator) { navigator.serviceWorker.register('./sw.js'); }
